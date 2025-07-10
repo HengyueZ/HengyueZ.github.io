854a30945bbf531b24e7871650d449153abce850
HengyueZ <67264128+HengyueZ@users.noreply.github.com>
2025-07-10 12:31:36 +0000

    Deploying to gh-pages from @ HengyueZ/HengyueZ.github.io@a72b5c4340c28af19849b33ee32ddb065032b385 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "This is my CV! For a PDF version, click the button on the top-right.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
